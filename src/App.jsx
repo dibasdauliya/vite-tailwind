0c3640c2e6665b36faf3c4e18b06e87fba89a8f6
Dibas Dauliya <dibasdauliya@gmail.com>
2022-06-10 22:05:19 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='p-5'>
+      <h1 className='text-4xl font-bold text-blue-500'>Vite + Tailwind</h1>
+      <p className='mt-4'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium,
+        est.
+      </p>
+    </div>
+  );
+}
+
+export default App;
